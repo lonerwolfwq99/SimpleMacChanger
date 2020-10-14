d4fd8feab1a337eaa1665d6172527d7ba11e0897
lonerwolfwq99 <41585568+lonerwolfwq99@users.noreply.github.com>
2020-10-13 17:52:44 -0700

    Add files via upload

---
+++ Simplemacchanger.py
@@ -0,0 +1,35 @@
+import subprocess
+import optparse # This option belongs to Options parse
+
+print(" ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ ")
+print("▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░ ")
+print("▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌")
+print("▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌▐░▌    ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌")
+print("▐░▌   ▄   ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▌   ▐░▌▐░▌          ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░ ")
+print("▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌")
+print(" ░▌ ▐░▌░▌ ▐░▌▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌   ▐░▌ ▐░▌▐░▌          ▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌")
+print(" ░▌▐░▌ ▐░▌▐░▌▐░▌     ▐░▌  ▐░▌          ▐░▌    ▐░▌▐░▌▐░▌          ▐░▌       ▐░▌          ▐░▌          ▐░▌")
+print("▐░▌░▌   ▐░▐░▌▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌ ▄▄▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄█░▌")
+print("▐░░▌     ▐░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌")
+print(" ▀▀       ▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ ")
+print ("--------------------------------------------------------------------------------------APT99------------")
+parser = optparse.OptionParser()     #parser knows everything
+
+parser.add_option("-i","--interface", dest="interface",help="what interface like etho , wlan0  to chang mach address")
+parser.add_option("-m","--mac", dest="new_mac",help="mac is 12 character for eg 00:11:12:33:22:11")
+(options,arguments) = parser.parse_args()
+
+interface = options.interface
+
+new_mac = options.new_mac
+
+print(" you mac address hve been changed to " + interface  + new_mac)
+
+
+#subprocess.call("ifconfig " +interface + " down", shell=True)
+#subprocess.call("ifconfig " +interface + " hw ether " +new_mac, shell=True) ---- NOT SECURED HIJACK OS COOMAND
+#subprocess.call("ifconfig " +interface + " up", shell=True)
+
+subprocess.call(["ifconfig", interface,"down"])
+subprocess.call(["ifconfig", interface,"hw","ether",new_mac])
+subprocess.call(["ifconfig", interface,"up"])
